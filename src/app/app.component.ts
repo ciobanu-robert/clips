db45e5641596cc29bf7e1abf1573c5e3de4cf35a
Robi <ciobanurobert100@gmail.com>
2024-01-05 22:02:03 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { UserModule } from './user/user.module';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    NavComponent,
+    UserModule, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'clips';
 }
---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-modal',
+  standalone: false,
+  templateUrl: './auth-modal.component.html',
+  styleUrl: './auth-modal.component.css'
+})
+export class AuthModalComponent {
+
+}
